3a4728f0cfae9c202f1dbd6971c106f9d1c20912
lucas william <lucaswilliamm14@gmail.com>
2026-08-20 20:57:56 -0300

    feat: aula 02 - primeiras rotas http

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,36 @@ import http from "node:http";
 
 const port = 3000;
 
+const routes = {
+    "/": {
+        mesage: "Restaurant Ordering System - API",
+    },
+    "/categorias": {
+        message : "Lista de categorias."
+
+    },
+    "/products": {
+        message : "Lista de produtos."
+    }
+}
+
 const server = http.createServer((req, res) => {
-    const response = {
-        message: "API Restaurant",
-        version: "1.0.0",
-        xpto: "qualquer coisa",
-    };
+
+    const response = routes[req.url as keyof typeof routes];
+       
+    if(!response){
+        res.writeHead(404,{
+            "content-type": "aplication/json",
+        });
+
+        return res.end(
+            JSON.stringify({
+                message: "Roude not found",
+            })
+
+        );
+    }
+
     res.writeHead(200, {
         "content-type": "application/json",
     });
